f599f398b3deec7585416401bac7bf7715a25089
Elijah Munroe <elimunroe15@hotmail.com>
2021-01-31 23:13:49 -0500

    Ask for user input

--- src/loopMap/Main.java
+++ src/loopMap/Main.java
@@ -5,7 +5,18 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
 		
+		System.out.println("Hello! Enter five (5) numbers to be summed, multiplied, maxed, and mined.");
+		for (int i = 0; i < 5; i++) {
+			int tempNum = 0;
+			
+			System.out.print("Enter a number (#" + i + 1 + "): " );
+			while (!scanner.hasNextInt()) {
+				tempNum = scanner.nextInt();
+			}
+			numbers.add(tempNum);
+		}
 	}
-
 }
