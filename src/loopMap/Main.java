00ae3e10136d9dd69001ac5ec2b225af378e266b
Elijah Munroe <elimunroe15@hotmail.com>
2021-01-31 23:33:34 -0500

    Derive sum, product, max, min of user inputted numbers

--- src/loopMap/Main.java
+++ src/loopMap/Main.java
@@ -7,16 +7,34 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		int tempNum = 0;
 		
-		System.out.println("Hello! Enter five (5) numbers to be summed, multiplied, maxed, and mined.");
+		System.out.println("Part 1: Please enter 5 numbers.");
 		for (int i = 0; i < 5; i++) {
-			int tempNum = 0;
-			
-			System.out.print("Enter a number (#" + i + 1 + "): " );
-			while (!scanner.hasNextInt()) {
-				tempNum = scanner.nextInt();
-			}
+			System.out.print("Enter a number (#" + (i + 1) + "): " );
+			tempNum = scanner.nextInt();
 			numbers.add(tempNum);
 		}
+		scanner.close();
+		
+		tempNum = numbers.get(0);
+		int sum = tempNum;
+		int product = tempNum;
+		int largest = tempNum;
+		int smallest = tempNum;
+		
+		for (int i = 1; i < 5; i++) {
+			tempNum = numbers.get(i);
+			sum += tempNum;
+			product *= tempNum;
+			if (largest < tempNum) {
+				largest = tempNum;
+			}
+			if (smallest > tempNum) {
+				smallest = tempNum;
+			}
+		}
+		
+		System.out.println("Sum: " + sum + "\tProduct: " + product + "\tLargest: " + largest + "\tSmallest: " + smallest);
 	}
 }
